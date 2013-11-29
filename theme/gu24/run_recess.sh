55e2824e05c9ef255c216642d46804f145d971ec
David Scotson <david.scotson@glasgow.ac.uk>
2013-11-29 10:26:15 +0000

    fixes some issues with rtl stripping code
    closes universityofglasgow/issues#31

--- theme/gu24/run_recess.sh
+++ theme/gu24/run_recess.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 recess --compile --compress  less/moodle.less > style/moodle.css
 sed -i 's/}/}\n/g' style/moodle.css
-sed -i 's/.dir-rtl.*,.dir-rtl/.dir-rtl/g' style/moodle.css
-sed -i '/^.dir-rtl/d' style/moodle.css
+sed -i 's/\(@media[^{]*\){/\1{\n/g' style/moodle.css
+sed -i 's/,.dir-rtl[^,]*,/,/g' style/moodle.css
+sed -i 's/^.dir-rtl[^,]*,//g' style/moodle.css
+sed -i 's/,.dir-rtl[^,]*{/{/g' style/moodle.css
+sed -i '/^[^ ,]*dir-rtl/d' style/moodle.css
