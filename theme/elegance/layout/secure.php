1325c8c49dbbdcd71edc4cf389f13aa01599fec8
Howard Miller <howardsmiller@gmail.com>
2014-11-19 10:12:35 +0000

    bodge up elegance theme with secure.php layout from bootstrap theme

--- theme/elegance/layout/secure.php
+++ theme/elegance/layout/secure.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of The Bootstrap 3 Moodle theme
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -14,61 +14,35 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * The Elegance theme is built upon  Bootstrapbase 3 (non-core).
- *
- * @package    theme
- * @subpackage theme_elegance
- * @author     Julian (@moodleman) Ridden
- * @author     Based on code originally written by G J Bernard, Mary Evans, Bas Brands, Stuart Lamour and David Scotson.
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 
+$hassidepre = $PAGE->blocks->region_has_content('side-pre', $OUTPUT);
 $hassidepost = $PAGE->blocks->region_has_content('side-post', $OUTPUT);
-$hascopyright = (empty($PAGE->theme->settings->copyright)) ? false : $PAGE->theme->settings->copyright;
-$hasfootnote = (empty($PAGE->theme->settings->footnote)) ? false : $PAGE->theme->settings->footnote;
-$hastiles = (!empty($PAGE->theme->settings->tiles));
-$haslogo = (empty($PAGE->theme->settings->logo)) ? false : $PAGE->theme->settings->logo;
-$invert = (!empty($PAGE->theme->settings->invert)) ? true : $PAGE->theme->settings->invert;
-$fluid = (!empty($PAGE->layout_options['fluid']));
 
- if ($haslogo) {
- 	$logo = '<div id="logo"></div>';
- } else {
- 	$logo = $SITE->shortname;
- }
-
- if ($invert) {
-  $navbartype = 'navbar-inverse';
-} else {
-  $navbartype = 'navbar-default';
-}
-
-$container = 'container';
-if (isset($PAGE->theme->settings->fluidwidth) && ($PAGE->theme->settings->fluidwidth == true)) {
-    $container = 'container-fluid';
-}
-if ($fluid) {
-    $container = 'container-fluid';
-}
-
+$knownregionpre = $PAGE->blocks->is_known_region('side-pre');
 $knownregionpost = $PAGE->blocks->is_known_region('side-post');
+
+$regions = bootstrap_grid($hassidepre, $hassidepost);
+$PAGE->set_popup_notification_allowed(false);
+if ($knownregionpre || $knownregionpost) {
+    theme_bootstrap_initialise_zoom($PAGE);
+}
+$setzoom = theme_bootstrap_get_zoom();
 
 echo $OUTPUT->doctype() ?>
 <html <?php echo $OUTPUT->htmlattributes(); ?>>
 <head>
     <title><?php echo $OUTPUT->page_title(); ?></title>
     <link rel="shortcut icon" href="<?php echo $OUTPUT->favicon(); ?>" />
-    <?php echo $OUTPUT->standard_head_html() ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php echo $OUTPUT->standard_head_html(); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimal-ui">
 </head>
 
-<body <?php echo $OUTPUT->body_attributes(); ?>>
+<body <?php echo $OUTPUT->body_attributes($setzoom); ?>>
 
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
 
-<nav role="navigation" class="navbar <?php echo $navbartype; ?>">
-    <div class="<?php echo $container; ?>">
+<nav role="navigation" class="navbar navbar-default">
+    <div class="container-fluid">
     <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#moodle-navbar">
             <span class="sr-only">Toggle navigation</span>
@@ -76,7 +50,7 @@ echo $OUTPUT->doctype() ?>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="<?php echo $CFG->wwwroot;?>"><?php echo $logo; ?></a>
+        <a class="navbar-brand" href="<?php echo $CFG->wwwroot;?>"><?php echo $SITE->shortname; ?></a>
     </div>
 
     <div id="moodle-navbar" class="navbar-collapse collapse">
@@ -88,35 +62,46 @@ echo $OUTPUT->doctype() ?>
     </div>
     </div>
 </nav>
-
-<section id="main" class="clearfix">
-    <div id="page" class="<?php echo $container; ?>">
-        <header id="page-header" class="clearfix">
-            <div id="course-header">
-                <?php echo $OUTPUT->course_header(); ?>
-            </div>
-        </header>
-    
-        <div id="page-content" class="row">
-            <div id="region-main" class="<?php echo $regions['content']; ?>">
-                <?php
-                echo $OUTPUT->course_content_header();
-                echo $OUTPUT->main_content();
-                echo $OUTPUT->course_content_footer();
-                ?>
-            </div>
-    
-            <?php
-            if ($knownregionpost) {
-                echo $OUTPUT->blocks('side-post', $regions['post']);
-            }?>
-        </div>
+<header class="moodleheader">
+    <div class="container-fluid">
+    <a href="<?php echo $CFG->wwwroot ?>" class="logo"></a>
+    <?php echo $OUTPUT->page_heading(); ?>
     </div>
-</section>
+</header>
 
-<footer id="page-footer">
-  <?php require_once(dirname(__FILE__).'/includes/footer.php'); ?>
-</footer>
 
+    <div id="page-content" class="row">
+        <div id="region-main" class="<?php echo $regions['content']; ?>">
+            <?php
+            echo $OUTPUT->course_content_header();
+
+            echo $OUTPUT->main_content();
+            echo $OUTPUT->course_content_footer();
+            ?>
+        </div>
+
+        <?php
+        if ($knownregionpre) {
+            echo $OUTPUT->blocks('side-pre', $regions['pre']);
+        }?>
+        <?php
+        if ($knownregionpost) {
+            echo $OUTPUT->blocks('side-post', $regions['post']);
+        }?>
+    </div>
+
+    <footer id="page-footer">
+        <div id="course-footer"><?php echo $OUTPUT->course_footer(); ?></div>
+        <p class="helplink"><?php echo $OUTPUT->page_doc_link(); ?></p>
+        <?php
+        echo $OUTPUT->login_info();
+        echo $OUTPUT->home_link();
+        echo $OUTPUT->standard_footer_html();
+        ?>
+    </footer>
+
+    <?php echo $OUTPUT->standard_end_of_body_html() ?>
+
+</div>
 </body>
 </html>
