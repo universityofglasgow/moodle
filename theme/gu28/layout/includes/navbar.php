4b4f50847ac01048ff376d0038bc0ce0d882f3a0
Howard Miller <howardsmiller@gmail.com>
2015-04-10 15:02:20 +0100

    gu28: adding glasgow logo to navbar

---
+++ theme/gu28/layout/includes/navbar.php
@@ -0,0 +1,22 @@
+
+<nav role="navigation" class="navbar">
+    <div class="<?php echo $container; ?>">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#moodle-navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="<?php echo $CFG->wwwroot;?>"><div id="logo"></div></a>
+    </div>
+
+    <div id="moodle-navbar" class="navbar-collapse collapse">
+        <?php echo $OUTPUT->custom_menu(); ?>
+        <?php echo $OUTPUT->user_menu(); ?>
+        <ul class="nav pull-right">
+            <li><?php echo $OUTPUT->page_heading_menu(); ?></li>
+        </ul>
+    </div>
+    </div>
+</nav>
