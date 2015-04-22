d04e00a28e87fa3f96253e3c4499a4d86b6defaa
Howard Miller <howardsmiller@gmail.com>
2015-04-22 14:34:09 +0100

    gu28: adding Quickmail

---
+++ blocks/quickmail/version.php
@@ -0,0 +1,7 @@
+<?php
+
+// Written at Louisiana State University
+$plugin->version = 2014042914;
+$plugin->requires = 2013051400;
+$plugin->release = "v1.5.0";
+$plugin->maturity = MATURITY_STABLE; 
