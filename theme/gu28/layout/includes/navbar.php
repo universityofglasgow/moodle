c184c50e041da8e5f7b94e96831ba9e0d7954652
Howard Miller <howardsmiller@gmail.com>
2015-08-20 16:06:39 +0100

    gu28: reflecting changes in Bootstrap update user dropdown (commit 0aa0d2a95f7813)

--- theme/gu28/layout/includes/navbar.php
+++ theme/gu28/layout/includes/navbar.php
@@ -1,23 +1,24 @@
-
 <nav role="navigation" class="navbar navbar-default">
     <div class="container-fluid">
-    <div class="navbar-header">
+    <div class="navbar-header pull-left">
+        <a class="navbar-brand" href="<?php echo $CFG->wwwroot;?>"><div id="logo"></div></a>
+        <div id="moodle-logo"></div>
+    </div>
+    <div class="navbar-header pull-right">
+        <?php if ($PAGE->pagelayout != 'login') {echo $OUTPUT->user_menu();} ?>
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#moodle-navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="<?php echo $CFG->wwwroot;?>"><div id="logo"></div></a>
-        <div id="moodle-logo"></div>
     </div>
-
-    <div id="moodle-navbar" class="navbar-collapse collapse">
+    <div id="moodle-navbar" class="navbar-collapse collapse navbar-right">
         <?php echo $OUTPUT->custom_menu(); ?>
-        <?php if ($PAGE->pagelayout != 'login') {echo $OUTPUT->user_menu();} ?>
         <ul class="nav pull-right">
             <li><?php echo $OUTPUT->page_heading_menu(); ?></li>
         </ul>
     </div>
+    
     </div>
 </nav>
