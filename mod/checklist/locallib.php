dc8b1e351bfa10e75113fddab860d916536f2632
Howard Miller <howardsmiller@gmail.com>
2018-05-18 13:33:51 +0100

    gu35: updated version of checklist

--- mod/checklist/locallib.php
+++ mod/checklist/locallib.php
@@ -26,8 +26,9 @@ use mod_checklist\local\checklist_comment;
 use mod_checklist\local\checklist_item;
 use mod_checklist\local\output_status;
 
-require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-require_once(dirname(__FILE__).'/lib.php');
+defined('MOODLE_INTERNAL') || die();
+global $CFG;
+require_once($CFG->dirroot.'/mod/checklist/lib.php');
 
 define("CHECKLIST_TEXT_INPUT_WIDTH", 45);
 define("CHECKLIST_OPTIONAL_NO", 0);
@@ -137,6 +138,14 @@ class checklist_class {
             $this->canlinkcourses = (bool)get_config('mod_checklist', 'linkcourses');
         }
         return $this->canlinkcourses;
+    }
+
+    /**
+     * Force checklist into 'edit dates' mode (really only needed by behat generator).
+     * @param bool $edit
+     */
+    public function set_editing_dates($edit) {
+        $this->editdates = (bool)$edit;
     }
 
     /**
@@ -1785,6 +1794,7 @@ class checklist_class {
                 $event->delete();
             } catch (dml_missing_record_exception $e) {
                 // Just ignore this error - the event is missing, so does not need deleting.
+                $event = null; // Do something here to stop codechecker complaining.
             }
             $item->eventid = 0;
             if ($add) {
@@ -1829,7 +1839,8 @@ class checklist_class {
         }
     }
 
-    protected function updateitem($itemid, $displaytext, $duetime = false, $linkcourseid = null, $linkurl = null, $grouping = null) {
+    protected function updateitem($itemid, $displaytext, $duetime = false, $linkcourseid = null, $linkurl = null,
+                                  $grouping = null) {
         $displaytext = trim($displaytext);
         if ($displaytext == '') {
             return;
