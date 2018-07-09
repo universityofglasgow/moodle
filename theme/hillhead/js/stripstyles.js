1e722043df698fc32a5c72e0cb0338f2232cfd7a
Alex Walker <alex.walker@glasgow.ac.uk>
2018-07-09 17:41:25 +0100

    StripStyles now works with more elements

--- theme/hillhead/js/stripstyles.js
+++ theme/hillhead/js/stripstyles.js
@@ -1,5 +1,5 @@
 require(['jquery'], function($) { $(document).ready(function(){
-   $("span").each(function() {
+   $("span, a, p, div, h1, h2, h3, h4, h5, h6").each(function() {
      $(this).removeAttr("style");
    });
 })});
