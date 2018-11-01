63f4b9ac289c45218dee0b8a0b3b93d8150cf8a4
Howard Miller <howardsmiller@gmail.com>
2018-11-01 16:29:38 +0000

    gu35: remember filter in enhancement report

--- report/enhance/amd/src/filter.js
+++ report/enhance/amd/src/filter.js
@@ -6,11 +6,17 @@ define(['jquery'], function($) {
             // Put whatever you like here. $ is available
             // to you as normal.
             $(".filter-list .nav-link").click(function() {
+                localStorage.setItem('selected-filter', $(this).attr("data-filter"));
                 $(".filter-list .nav-link").removeClass("active");
                 $(this).addClass("active");
                 $(".filter-all").hide();
                 $($(this).attr("data-filter")).show();
             });
+
+            if (filter = localStorage.getItem('selected-filter')) {
+                $(".filter-all").hide();
+                $(filter).show();
+            }
         }
     };
 });
