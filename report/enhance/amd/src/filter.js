322afaeb03a318131849a9e56e54d2111a6c6343
Howard Miller <howardsmiller@gmail.com>
2018-11-06 15:11:07 +0000

    gu35: report_enhance grunt the amd module

--- report/enhance/amd/src/filter.js
+++ report/enhance/amd/src/filter.js
@@ -1,8 +1,8 @@
 define(['jquery'], function($) {
- 
+
     return {
         init: function() {
- 
+
             // Put whatever you like here. $ is available
             // to you as normal.
             $(".filter-list .nav-link").click(function() {
@@ -13,7 +13,8 @@ define(['jquery'], function($) {
                 $($(this).attr("data-filter")).show();
             });
 
-            if (filter = localStorage.getItem('selected-filter')) {
+            var filter = localStorage.getItem('selected-filter');
+            if (filter) {
                 $(".filter-all").hide();
                 $(filter).show();
             }
