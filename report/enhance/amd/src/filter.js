d4ed9620078dfce578458bb3bad0f4aa6289ea67
Alex Walker <alex.walker@glasgow.ac.uk>
2019-01-16 15:17:20 +0000

    Enhancement Report Fixes
    - Filter list degrades gracefully if Local Storage is unavailable
    - Enhancements now show ID numbers
    - Bottom ‘Add Request’ button doesn’t show if filter list is empty
    - No one else can feel it for you
    - Only you can let it in
    - No one else, no one else can speak the words on your lips

--- report/enhance/amd/src/filter.js
+++ report/enhance/amd/src/filter.js
@@ -6,17 +6,35 @@ define(['jquery'], function($) {
             // Put whatever you like here. $ is available
             // to you as normal.
             $(".filter-list .nav-link").click(function() {
-                localStorage.setItem('selected-filter', $(this).attr("data-filter"));
                 $(".filter-list .nav-link").removeClass("active");
                 $(this).addClass("active");
                 $(".filter-all").hide();
                 $($(this).attr("data-filter")).show();
+                if($($(this).attr("data-filter")).length == 0) {
+                    $(".bottom-button").hide();
+                } else {
+                    $(".bottom-button").show();
+                }
+                try {
+                    localStorage.setItem('selected-filter', $(this).attr("data-filter"));
+                } catch (error) {
+                    return false;
+                }
             });
 
-            var filter = localStorage.getItem('selected-filter');
-            if (filter) {
-                $(".filter-all").hide();
-                $(filter).show();
+            try {
+                var filter = localStorage.getItem('selected-filter');
+                if (filter) {
+                    $(".filter-all").hide();
+                    $(filter).show();
+                    if($(filter).length == 0) {
+                        $(".bottom-button").hide();
+                    } else {
+                        $(".bottom-button").show();
+                    }
+                }
+            } catch (error) {
+                return false;
             }
         }
     };
