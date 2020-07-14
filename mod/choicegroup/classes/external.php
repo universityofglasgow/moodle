752c3b94c581e4cbbf83fe5624af3d63d097622f
Howard Miller <howardsmiller@gmail.com>
2020-07-14 10:35:39 +0100

    gu38: updated versions of plugins

--- mod/choicegroup/classes/external.php
+++ mod/choicegroup/classes/external.php
@@ -79,7 +79,7 @@ class mod_choicegroup_external extends external_api {
         require_capability('mod/choicegroup:choose', $context);
 
         $allresponses = choicegroup_get_response_data($choicegroup, $cm);   // Big function, approx 6 SQL calls per user
-        $answers = choicegroup_get_user_answer($choicegroup, $userid, TRUE);
+        $answers = choicegroup_get_user_answer($choicegroup, $userid, true);
 
         foreach ($choicegroup->option as $optionid => $text) {
             if (isset($text)) {
@@ -136,12 +136,13 @@ class mod_choicegroup_external extends external_api {
                     new external_single_structure(
                         array(
                             'id' => new external_value(PARAM_INT, 'Option id'),
-                            'text' => new external_value(PARAM_RAW, 'Group choice name'),
+                            'groupid' => new external_value(PARAM_INT, 'Group id'),
+                            'name' => new external_value(PARAM_RAW, 'Group choice name'),
                             'maxanswers' => new external_value(PARAM_INT, 'Maximum number of accepted answers', VALUE_OPTIONAL),
                             'displaylayout' => new external_value(PARAM_INT, 'Display layout', VALUE_OPTIONAL),
                             'countanswers' => new external_value(PARAM_INT, 'Current number of answers', VALUE_OPTIONAL),
-                            'checked' => new external_value(PARAM_INT, 'Checked', VALUE_OPTIONAL),
-                            'disabled' => new external_value(PARAM_INT, 'Disabled', VALUE_OPTIONAL),
+                            'checked' => new external_value(PARAM_BOOL, 'Checked', VALUE_OPTIONAL),
+                            'disabled' => new external_value(PARAM_BOOL, 'Disabled', VALUE_OPTIONAL),
                         )
                     )
                 ),
@@ -412,7 +413,7 @@ class mod_choicegroup_external extends external_api {
             throw new moodle_exception('expired', 'choicegroup', '', userdate($choice->timeclose));
         }
 
-        $answergiven = choicegroup_get_user_answer($choicegroup, $USER, TRUE);
+        $answergiven = choicegroup_get_user_answer($choicegroup, $USER, true);
         if (!empty($answergiven)) {
             if ($choicegroup->allowupdate && !$choicegroup->multipleenrollmentspossible) {
                 $params = array('groupid' => reset($answergiven)->id, 'userid' => $USER->id);
