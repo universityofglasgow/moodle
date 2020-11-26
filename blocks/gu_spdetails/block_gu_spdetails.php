bce57668065d19afc426d9bf2daa4767a3d9e741
francolouietm <francolouie@gmail.com>
2020-11-26 16:26:53 +0800

    Initial block plugin files for 'Your assessments details'

---
+++ blocks/gu_spdetails/block_gu_spdetails.php
@@ -0,0 +1,64 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Contains the class for the UofG Assessments Details block.
+ *
+ * @package    block_gu_spdetails
+ * @copyright  2020 Accenture
+ * @author     Franco Louie Magpusao <franco.l.magpusao@accenture.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+class block_gu_spdetails extends block_base {
+
+    public function init() {
+        $this->title = get_string('pluginname', 'block_gu_spdetails');
+    }
+
+    public function hide_header() {
+        return TRUE;
+    }
+
+    /**
+     * Returns the contents.
+     *
+     * @return stdClass contents of block
+     * @todo Create a renderer for assessments block.
+     *       See myoverview plugin for reference.
+     */
+    public function get_content() {
+        if ($this->content !== null) {
+          return $this->content;
+        }
+
+        $this->content         = new stdClass();
+        $this->content->text   = get_string('sample_text', 'block_gu_spdetails');
+     
+        return $this->content;
+    }
+
+    /**
+     * Locations where block can be displayed.
+     *
+     * @return array
+     */
+    public function applicable_formats() {
+        return array('my' => true);
+    }
+}
