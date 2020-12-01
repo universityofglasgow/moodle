c13e551c93471a3a1f40e032e5882df7ca960028
deguzman22 <74400168+deguzman22@users.noreply.github.com>
2020-12-01 16:58:46 +0800

    Feature portal38 woadd 513 (#25)
    * Initial plugin files for GU Student Portal
    
    * JIRA-513 :: [GCAT] Create a blocks for assessment at a glance
    
    * JIRA-513 :: [Portal] Renamed branch
    
    * JIRA-513 :: [Portal] Removed assessmentglance block
    
    * JIRA-513 :: [Portal] Add has_config method
    
    * JIRA-513 :: [Portal] Add fonts folder
    
    * JIRA-513 :: [Portal] Add external css file for blocks
    
    * Add mustache file
    
    * JIRA-513 :: [Portal] Apply new UI
    
    * JIRA-513 :: [Portal] Added new image
    
    * JIRA-513 :: [Portal] Removed internal svg and called external svg
    
    * JIRA-513 :: [Portal] Refactored code
    
    * JIRA-513 :: [Portal] Removed custom plugin
    
    * JIRA-513 :: [Portal] Changed including css file
    
    * JIRA-513 :: [Portal] Removed my changes
    
    Co-authored-by: francolouietm <francolouie@gmail.com>

--- blocks/gu_spoverview/block_gu_spoverview.php
+++ blocks/gu_spoverview/block_gu_spoverview.php
@@ -36,6 +36,15 @@ class block_gu_spoverview extends block_base {
     }
 
     /**
+     * Allow the block to have a configuration page
+     *
+     * @return boolean
+     */
+    public function has_config() {
+        return true;
+    }
+
+    /**
      * Returns the contents.
      *
      * @return stdClass contents of block
@@ -43,13 +52,94 @@ class block_gu_spoverview extends block_base {
      *       See myoverview plugin for reference.
      */
     public function get_content() {
-        if ($this->content !== null) {
-          return $this->content;
+        global $USER, $DB, $PAGE, $OUTPUT;
+
+
+        $PAGE->requires->css('/blocks/gu_spoverview/styles.css');
+
+        $student_id = $USER->id;
+        $current_yr = date("Y");
+
+        // Query for all the assignments whose status was submitted
+        $assigment_submitted    = $DB->get_records_sql("SELECT * FROM `mdl_assign_submission` 
+                                                        WHERE FROM_UNIXTIME(timecreated, '%Y') = $current_yr 
+                                                        AND userid = $student_id AND status='submitted'");
+
+        // Query for all assignments for a specific student
+        $assignments            = $DB->get_records_sql("SELECT DISTINCT i.itemname 
+                                                        FROM `mdl_course_modules` cm 
+                                                        INNER JOIN `mdl_modules` m ON m.id = cm.module 
+                                                        INNER JOIN `mdl_grade_items` i ON i.courseid = cm.course
+                                                        INNER JOIN 	`mdl_assign` ass ON ass.name = i.itemname 
+                                                        INNER JOIN `mdl_grade_grades` g ON g.itemid = i.id 
+                                                        INNER JOIN `mdl_user` u ON u.id = g.userid 
+                                                        WHERE m.name IN ('assign') 
+                                                        AND i.itemmodule IS NOT NULL AND u.id = $student_id 
+                                                        AND FROM_UNIXTIME(i.timemodified, '%Y') = $current_yr"); 
+
+        // Query for all the assignments that were not yet submit and overdue
+        $assignment_duedate     = $DB->get_records_sql("SELECT  DISTINCT i.itemname
+                                                        FROM `mdl_course_modules` cm 
+                                                        INNER JOIN `mdl_modules` m ON m.id = cm.module 
+                                                        INNER JOIN `mdl_grade_items` i ON i.courseid = cm.course 
+                                                        INNER JOIN `mdl_grade_grades` g ON g.itemid = i.id
+                                                        INNER JOIN `mdl_user` u ON u.id = g.userid
+                                                        INNER JOIN 	`mdl_assign` ass ON ass.name = i.itemname
+                                                        INNER JOIN `mdl_grade_grades` gg ON gg.userid = $student_id
+                                                        INNER JOIN `mdl_assign_submission` asub ON (asub.assignment = ass.id AND asub.status <> 'submitted')  
+                                                        WHERE m.name IN ('assign') 
+                                                        AND i.itemmodule IS NOT NULL AND u.id = $student_id
+                                                        AND FROM_UNIXTIME(i.timemodified, '%Y') = $current_yr 
+                                                        AND FROM_UNIXTIME(ass.duedate) < NOW()
+                                                        AND gg.finalgrade IS NULL");
+
+        // Query for all the assignments, with a grade and with feedback
+        $assign_marked_assessment_count = $DB->get_records_sql("SELECT DISTINCT i.itemname 
+                                                                FROM `mdl_course_modules` cm 
+                                                                INNER JOIN `mdl_modules` m ON m.id = cm.module 
+                                                                INNER JOIN `mdl_grade_items` i ON i.courseid = cm.course 
+                                                                INNER JOIN `mdl_assign` ass ON ass.name = i.itemname 
+                                                                INNER JOIN `mdl_grade_grades` g ON g.itemid = i.id 
+                                                                INNER JOIN `mdl_user` u ON u.id = g.userid 
+                                                                WHERE m.name IN ('assign') 
+                                                                AND i.itemmodule IS NOT NULL AND u.id = $student_id 
+                                                                AND FROM_UNIXTIME(i.timemodified, '%Y') = $current_yr 
+                                                                AND g.finalgrade IS NOT NULL 
+                                                                AND g.feedback IS NOT NULL");
+
+        // Query for all the quiz, with a grade and with feedback
+        $quiz_marked_assessment_count = $DB->get_records_sql("SELECT DISTINCT i.itemname 
+                                                                FROM `mdl_course_modules` cm 
+                                                                INNER JOIN `mdl_modules` m ON m.id = cm.module 
+                                                                INNER JOIN `mdl_grade_items` i ON i.courseid = cm.course 
+                                                                INNER JOIN `mdl_quiz` q ON q.name = i.itemname 
+                                                                INNER JOIN `mdl_grade_grades` g ON g.itemid = i.id 
+                                                                INNER JOIN `mdl_user` u ON u.id = g.userid 
+                                                                WHERE m.name IN ('quiz') 
+                                                                AND i.itemmodule IS NOT NULL AND u.id = $student_id 
+                                                                AND FROM_UNIXTIME(i.timemodified, '%Y') = $current_yr 
+                                                                AND g.finalgrade IS NOT NULL 
+                                                                AND g.feedback IS NOT NULL");
+
+        if($this->content !== NULL) {
+            return $this->content;
         }
 
-        $this->content         = new stdClass();
-        $this->content->text   = get_string('sample_text', 'block_gu_spoverview');
-     
+        $templatecontext = (object)[
+            'assignment_submitted_count' => sizeof($assigment_submitted),
+            'to_be_submitted'            => (int)sizeof($assignments) - (int)sizeof($assigment_submitted),
+            'due_date'                   => (int)sizeof($assignment_duedate),
+            'marked_assessment_count'    => (int)sizeof($assign_marked_assessment_count) + (int)sizeof($quiz_marked_assessment_count),
+            'assignment_svg_path'        => '../blocks/gu_spoverview/pix/assignment-submitted.svg',
+            'to_be_submitted_svg_path'   => '../blocks/gu_spoverview/pix/to-be-submitted.svg',
+            'overdue_svg_path'           => '../blocks/gu_spoverview/pix/overdue.svg',
+            'assessment_marked_svg_path' => '../blocks/gu_spoverview/pix/assessment-marked.svg'
+        ];
+
+        $this->content = new stdClass;
+
+        $this->content->text = $OUTPUT->render_from_template('block_gu_spoverview/spoverview', $templatecontext);
+
         return $this->content;
     }
 
