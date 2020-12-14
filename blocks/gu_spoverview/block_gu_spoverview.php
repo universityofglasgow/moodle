bada64be6e1cef7a36fcccfe8e2ff0ec62af9f76
franco <francolouietm@users.noreply.github.com>
2020-12-14 22:30:47 +0800

    Update weight value and fix warnings (#72)

--- blocks/gu_spoverview/block_gu_spoverview.php
+++ blocks/gu_spoverview/block_gu_spoverview.php
@@ -142,7 +142,7 @@ class block_gu_spoverview extends block_base {
         $params = array($userid, 'mod');
         $incourseids = implode(',', $courseids);
 
-        $sql = "SELECT mgi.itemname, mgg.finalgrade
+        $sql = "SELECT DISTINCT mgi.id, mgi.itemname, mgg.finalgrade
                 FROM `mdl_grade_items` mgi
                 JOIN `mdl_grade_grades` mgg ON mgi.id = mgg.itemid AND mgg.userid = ? AND mgg.finalgrade IS NOT NULL
                 WHERE mgi.itemtype = ? AND mgi.courseid IN (".$incourseids.")";
