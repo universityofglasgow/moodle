fde8597ef9dc879be8b480399447d2b099841dc5
deguzman22 <74400168+deguzman22@users.noreply.github.com>
2021-01-20 13:41:58 +0800

    Feature gcat38 woadd 909 (#214)
    * Fixed breadcrumbs
    
    * Removed unused code
    
    * Fixed breadcrumb
    
    * Removed unused code

--- local/gugcat/add/index.php
+++ local/gugcat/add/index.php
@@ -38,8 +38,6 @@ $urlparams = array('id' => $courseid, 'activityid' => $activityid, 'studentid' =
 $URL = new moodle_url('/local/gugcat/add/index.php', $urlparams);
 $PAGE->set_url($URL);
 $PAGE->set_title(get_string('gugcat', 'local_gugcat'));
-$PAGE->navbar->ignore_active();
-$PAGE->navbar->add(get_string('navname', 'local_gugcat'), $URL);
 
 $PAGE->requires->css('/local/gugcat/styles/gugcat.css');
 $PAGE->requires->js_call_amd('local_gugcat/main', 'init');
@@ -53,7 +51,6 @@ $PAGE->set_heading($course->fullname);
 
 $student = $DB->get_record('user', array('id'=>$studentid, 'deleted'=>0), '*', MUST_EXIST);
 $module = local_gugcat::get_activities($courseid)[$activityid];
-$PAGE->set_cm($module);
 
 $scaleid = $module->gradeitem->scaleid;
 if (is_null($scaleid) && local_gugcat::is_grademax22($module->gradeitem->gradetype, $module->gradeitem->grademax)){
@@ -88,9 +85,8 @@ if ($fromform = $mform->get_data()) {
 }   
 
 echo $OUTPUT->header();
+$PAGE->set_cm($module);
 $renderer = $PAGE->get_renderer('local_gugcat');
 echo $renderer->display_add_edit_grade_form($course, $student, $gradeversions, true);
 $mform->display();
 echo $OUTPUT->footer();
-
-
