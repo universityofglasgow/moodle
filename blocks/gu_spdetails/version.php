c1f7f59fa29724a4129d2c96aac46adfa1d05143
franco <francolouietm@users.noreply.github.com>
2021-01-21 21:40:11 +0800

    Update block versions (#251)
    * Update blocks plugin version
    
    * Update Assessments at a Glance block version

--- blocks/gu_spdetails/version.php
+++ blocks/gu_spdetails/version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021012106;
+$plugin->version   = 2021012106.01;
 $plugin->requires  = 2019111804.11;                  // Moodle 3.8.4.
 $plugin->component = 'block_gu_spdetails';
