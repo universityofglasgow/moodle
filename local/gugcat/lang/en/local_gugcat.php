7f2a3ce68fc2740a920930b8c51b099b04fe8835
m-aquino <74167293+m-aquino@users.noreply.github.com>
2021-03-10 16:49:56 +0800

    Feature gcat38 fixes (#425)
    * Blindmarking and downloadcsv fixes
    
    * Added new function for retrieving students
    
    * Fixes for 3 defects
    
    * Revert scaleid check
    
    * Fixed final released
    
    * Custom field visibility

--- local/gugcat/lang/en/local_gugcat.php
+++ local/gugcat/lang/en/local_gugcat.php
@@ -38,7 +38,6 @@ $string['assessmentgradehistory'] = 'Assessment Grade History';
 $string['cancelmodal'] = 'No, cancel action';
 $string['candidateno'] = 'Candidate No.';
 $string['choosegrade'] = 'Choose grade';
-$string['configdata'] = '{"required":"0","uniquevalues":"0","checkbydefault":"0","locked":"0","visibility":"1"}';
 $string['confirmgrade'] = 'Confirm new grade';
 $string['confirmchanges'] = 'Yes, save changes';
 $string['confirmfinalrelease'] = 'Yes, release final assessment grades';
