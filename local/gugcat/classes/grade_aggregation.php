a975e6ee04462306dd399726041beeabe14ce9fa
natividadelijah <74233170+natividadelijah@users.noreply.github.com>
2021-05-05 19:05:31 +0800

    Feature gcat38 woadd 1570 (#567)
    * fix for WOADD-1570, 1571, 1572, 1573, and 1574
    
    * fix for 1571 and 1573
    
    * removed var_dump

--- local/gugcat/classes/grade_aggregation.php
+++ local/gugcat/classes/grade_aggregation.php
@@ -949,8 +949,8 @@ class grade_aggregation{
             foreach($prvgrades as $prvgrd){
                 $componentnotes = preg_replace('/ \-./i', '', $notes);
                 $is_scale = !is_null($prvgrd->idnumber) ? $prvgrd->idnumber : false;
-                $notes = $is_scale ? $componentnotes . " -" . $is_scale : $componentnotes;
-                local_gugcat::update_components_notes($subcatgrd->userid, $prvgrd->id, $notes);
+                $newnotes = $is_scale ? $componentnotes . " -" . $is_scale : $componentnotes;
+                local_gugcat::update_components_notes($subcatgrd->userid, $prvgrd->id, $newnotes);
             }
         }
     }
