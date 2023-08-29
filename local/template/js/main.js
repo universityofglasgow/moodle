1102ac391a8e2f8af877470d5ce0262f9d69fcce
David Aylmer <david.aylmer@glasgow.ac.uk>
2023-08-29 07:57:18 +0100

    local template fixes

--- local/template/js/main.js
+++ local/template/js/main.js
@@ -22,7 +22,6 @@ var sliderinitcount = 0;
  * Wait until the page is loaded before initialising.
  */
 $(document).ready(function() {
-    debugger;
     local_template_init();
 });
 
@@ -30,7 +29,6 @@ $(document).ready(function() {
  * Initialise.
  */
 function local_template_init() {
-    debugger;
 
     // If Stepper exists, initialise stepper.
     if ($(templateselector).length) {
@@ -40,7 +38,6 @@ function local_template_init() {
     // If sliders exist, register event for when sliders finish loading, and initialise sliders.
     if ($(sliderselector).length) {
         $(sliderselector).on('init', function (event, slick) {
-            debugger;
             sliderinitcount += 1;
             // Have all sliders been initialised?
             if ($(sliderselector).length === sliderinitcount) {
@@ -58,7 +55,6 @@ function local_template_init() {
  * Initialise the stepper component.
  */
 function local_template_initStepper() {
-    debugger;
 
     // Initialise stepper.
     if ($(templateselector).length) {
@@ -78,11 +74,9 @@ function local_template_initStepper() {
  * Initialise the slick sliders.
  */
 function local_template_initSliders() {
-    debugger;
 
     // Initialise all the sliders.
     $(sliderselector).each(function() {
-        debugger;
         var slider = $(this);
         // Hide the sliders and show the loading spinner.
         slider.css('opacity', 0.01).slideUp();
@@ -94,7 +88,6 @@ function local_template_initSliders() {
         /*
                 $.when(slider.not('.slick-initialized').slick()).then(
                     waitForFinalEvent(function() {
-                        debugger;
                         showSlider(slider);
                         slider.slideUp();
                     }, 2000, "showSlider" + slider.attr("id"))
@@ -106,7 +99,6 @@ function local_template_initSliders() {
     /*
         elementLoaded('.slick-initialized', $('.slider').length, function(element) {
             // Element is ready to use.
-            debugger;
             registerMouseEvents();
         });
      */
@@ -119,7 +111,6 @@ function local_template_initSliders() {
 function local_template_registerEvents() {
     // document or body?
     $('body').on('click', usetemplateselector, function() {
-        debugger;
 
         var value = $(this).attr("data-id");
         var inputselector = idtemplatecourseselector + ' .form-autocomplete-suggestions li[data-value="' + value + '"]';
@@ -138,9 +129,7 @@ function local_template_registerEvents() {
     });
 
     $("#templatesteppertrigger1").on('click', function() {
-        debugger;
         $(sliderselector).each(function () {
-            debugger;
             var slider = $(this);
             slider[0].slick.refresh();
             local_template_resizeFixes(slider);
@@ -148,7 +137,6 @@ function local_template_registerEvents() {
     });
 
     $("h1.category-heading").on('click', function() {
-        debugger;
         var categoryheading = $(this);
         var carddeck = categoryheading.nextAll('.card-deck:first');
         if (!carddeck.hasClass('slider')) return;
@@ -170,15 +158,12 @@ function local_template_registerEvents() {
 /*
 // Wait for element to exist.
 function local_template_elementLoaded(element, count, callback) {
-    debugger;
     if ($(element).length == count) {
-        debugger;
         // All elements loaded.
         callback($(element));
     } else {
         // Repeat every 500ms.
         setTimeout(function() {
-            debugger;
             elementLoaded(element, count, callback)
         }, 50);
     }
@@ -187,7 +172,6 @@ function local_template_elementLoaded(element, count, callback) {
 
 /*
 var waitForFinalEvent = (function() {
-    debugger;
     var timers = {};
     return function(callback, ms, uniqueId) {
         if (!uniqueId) {
@@ -202,15 +186,12 @@ var waitForFinalEvent = (function() {
 */
 
 function local_template_resizeFixes(slider, animate) {
-    debugger;
     slider.find('.slick-track').each(function() {
-        debugger;
         local_template_fixTrackHeight(this, animate);
     });
 }
 
 function local_template_showSlider(slider) {
-    debugger;
     var spinner = slider.parent().find(".spinner");
 
     slider.show();
@@ -226,7 +207,6 @@ function local_template_showSlider(slider) {
 
     // For each card in the slider, hide the body and footer.
     slider.find('.card').each(function() {
-        // debugger;
         var card = $(this);
         var cardbody = card.find('div.card-body');
         var cardoverlay = card.find('div.card-img-overlay');
@@ -238,18 +218,16 @@ function local_template_showSlider(slider) {
 function local_template_registerSliderEvents(track) {
     // Add listeners to document for slick slide mouseenter.
     // document or body?
-    debugger;
 
     // Add listeners for slick slide mouseleave.
     $(track).on('mouseleave', '.slick-slide', function() {
-        debugger;
         var slide = $(this);
         var card = slide.find('div.card');
         var cardbody = card.find('div.card-body');
         var cardoverlay = card.find('div.card-img-overlay');
 
         // On mouseleave, return the current slide up to 75% opacity, and reset the scale to 1x.
-        slide.css('animation-delay', '20ms');
+        slide.css('animation-delay', '0s');
         slide.css('animation-name', 'fadeout-scaledown');
 
         // Use jquery transitions for hiding cardbody.
@@ -258,14 +236,13 @@ function local_template_registerSliderEvents(track) {
     });
 
     $(track).on('mouseenter', '.slick-slide', function() {
-        debugger;
         var slide = $(this);
         var card = slide.find('div.card');
         var cardbody = card.find('div.card-body');
         var cardoverlay = card.find('div.card-img-overlay');
 
         // On mouseenter, bring the current slide up to 100% opacity, and scale by 1.2x.
-        slide.css('animation-delay', '20ms');
+        slide.css('animation-delay', '50ms');
         slide.css('animation-name', 'fadein-scaleup');
 
         // Use jquery transitions for revealing cardbody and cardfooter.
@@ -275,7 +252,6 @@ function local_template_registerSliderEvents(track) {
 }
 
 function local_template_fixTrackHeight(track, animate) {
-    debugger;
     var list = $(track).parent();
     var prev = list.parent().find(".slick-prev");
     var next = list.parent().find(".slick-next");
