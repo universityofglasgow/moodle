5d748144f8205a03eb383a2213d91e6e4297e2d7
Ferenc-Frank-Lengyel <ferenc.lengyel@glasgow.ac.uk>
2024-04-30 11:23:56 +0100

    update local_mymedia

--- local/mymedia/version.php
+++ local/mymedia/version.php
@@ -25,11 +25,11 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');
 }
 
-$plugin->version = 2022112803;
+$plugin->version = 2023100901;
 $plugin->component = 'local_mymedia';
-$plugin->release = 'Kaltura release 4.4.6';
-$plugin->requires = 2022112800;
+$plugin->release = 'Kaltura release 4.4.8';
+$plugin->requires = 2023100901;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'local_kaltura' => 2022112803
+    'local_kaltura' => 2023100901
 );
