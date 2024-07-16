1830691fdc397e728f5d23ce61e958d119d9d852
Ferenc-Frank-Lengyel <ferenc.lengyel@glasgow.ac.uk>
2024-07-16 11:26:54 +0100

    update mod_attendance

--- mod/attendance/version.php
+++ mod/attendance/version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2024060401;
-$plugin->release = 2024060401;
+$plugin->version  = 2024060402;
+$plugin->release = 2024060402;
 $plugin->requires = 2024041200; // Requires 4.4.
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
