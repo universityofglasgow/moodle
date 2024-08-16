97330539b17719941d6d09e356558cb2756d3255
Greg Pedder <greg.pedder@glasgow.ac.uk>
2024-08-16 16:15:12 +0100

    MGU-924 - Rewrote readtome.js as jQuery no longer recommended by Moodle. Also fixes the issue whereby not all elements were being picked up by the selector search, this was because they weren't on the page at the time it ran. Using a timer to wait for the elements to appear. Modified the template to include further aria attributes. Change to the canvas tag for accessibility purposes made also. Still a WIP.

--- theme/hillhead40/js/readtome.js
+++ theme/hillhead40/js/readtome.js
@@ -1,32 +1,50 @@
-require(['jquery'], function($) { $(document).ready(function(){
-    if(typeof window.speechSynthesis !== 'undefined') {
-		$('p, h1, h2, h3, h4, h5, span, a, label, button, strong, canvas').hover(
-    	    function() {
-        	    $(this).addClass('currentlySpeaking');
-        	    $(this).addClass('currentlySpeakingHighlight');
-        	    if(typeof speakTimer !== 'undefined') {
-        	        clearTimeout(speakTimer);
-        	    }
-        	    window.speechSynthesis.cancel();
-        	    var speakText = $(this).text();
-        	    var $hillheadCurrentlySpeaking = $(this);
-        	    speakTimer = setTimeout( function() {
-            		var msg = new SpeechSynthesisUtterance(speakText);
-                	msg.lang = 'en-gb';
-                	msg.onend = function(event) {
-                    	 $hillheadCurrentlySpeaking.removeClass('currentlySpeaking');
-                         $hillheadCurrentlySpeaking.removeClass('currentlySpeakingHighlight');
-                	};
-            		window.speechSynthesis.speak(msg);
-                }, 1500);
-            },
-            function() {
-                window.speechSynthesis.cancel();
-                $(this).removeClass('currentlySpeaking');
-                $(this).removeClass('currentlySpeakingHighlight');
-            }
-        );
-    } else {
-        $('<div class="alert alert-danger">Read To Me doesn\'t work in Internet Explorer because it doesn\'t support Text To Speech. We recommend you use Google Chrome or Firefox instead if you want to use Read To Me.</div>').insertBefore("#page-content");
+/**
+ * Reimplenting this script using plain ol javascript as Moodle doesn't want us using jQuery anymore.
+ * Basically, when Read-To-Me has been turned on, this should search the page for particular elements
+ * and, using the text value, run these through the browser's text to speech feature. The async approach
+ * now gets us around the (subtle) problem with the previous version by waiting for ^all^ the content to
+ * have loaded, before starting the search. See Student MyGrades for an example, the 'dashboard' panel
+ * loads separately to the rest of the page.
+ * @param {int} timeout
+ * @returns {Promise<*|null>}
+ */
+async function waitForElement(timeout = 2000) {
+    console.log('waitForElement called...');
+    const start = Date.now();
+
+    while (Date.now() - start < timeout) {
+        if(typeof window.speechSynthesis !== 'undefined') {
+			document.querySelectorAll('p, h1, h2, h3, h4, h5, span, a, label, button, strong, canvas, table>thead>tr>th, a[href*="downloadspdetails.php"]>i').forEach((v) => {
+				v.addEventListener('mouseover', (e) => {
+					e.target.classList.add('currentlySpeaking', 'currentlySpeakingHighlight');
+					if(typeof speakTimer !== 'undefined') {
+						clearTimeout(speakTimer);
+					}
+					window.speechSynthesis.cancel();
+					let speakText = e.target.textContent;
+        	        let $hillheadCurrentlySpeaking = e.target;
+        	        speakTimer = setTimeout( function() {
+            		    var msg = new SpeechSynthesisUtterance(speakText);
+                	    msg.lang = 'en-gb';
+                	    msg.onend = function(event) {
+                    	    $hillheadCurrentlySpeaking.classList.remove('currentlySpeaking', 'currentlySpeakingHighlight');
+                	    };
+            		    window.speechSynthesis.speak(msg);
+                    }, 1500);
+				});
+				v.addEventListener('mouseout', (e) => {
+					window.speechSynthesis.cancel();
+					e.target.classList.remove('currentlySpeaking', 'currentlySpeakingHighlight');
+				});
+			  });
+		} else {
+			let tempPanel = document.querySelector('#page-content');
+			tempPanel.insertAdjacentHTML("beforebegin", '<div class="alert alert-danger">Read To Me is unavailable in your browser as it doesn\'t appear to support Text To Speech. We recommend you use Google Chrome or Firefox instead if you want to use Read To Me.</div>');
+		}
+        await new Promise(resolve => setTimeout(resolve, 1000));
     }
-})});
+
+    return null;
+}
+
+waitForElement(2500);
