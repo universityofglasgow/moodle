9b2fdb8ee20f9891ba898f5af46ff319a353718d
Greg Pedder <greg.pedder@glasgow.ac.uk>
2024-10-30 13:51:45 +0000

    Fix for grade category that's been processed via MyGrades, needing a released and releasegrade value.

--- blocks/newgu_spdetails/classes/course.php
+++ blocks/newgu_spdetails/classes/course.php
@@ -159,7 +159,11 @@ class course {
                     $subcat->assessment_type = $assessmenttype;
                     $subcat->sub_category_weight = $subcatweight;
                     $subcat->raw_category_weight = $rawsubcatweight;
-                    $subcat->grade_category_grade = $gradecategory['display'];
+                    if ($gradecategory['releasegrade'] == true) {
+                        if (!$gradecategory['grademissing']) {
+                            $subcat->grade_category_grade = $gradecategory['display'];
+                        }
+                    }
 
                     $gradessubcatdata[] = $subcat;
                 }
