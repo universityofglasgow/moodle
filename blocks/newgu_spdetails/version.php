b1acd42ef84853a6b8c579f9edf37439ba8554cb
Greg Pedder <greg.pedder@glasgow.ac.uk>
2024-11-18 09:09:17 +0000

    MGU-1183 - Version bump. Last commit changes not coming through.

--- blocks/newgu_spdetails/version.php
+++ blocks/newgu_spdetails/version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'block_newgu_spdetails';
-$plugin->version = 2024111501;
+$plugin->version = 2024111801;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '0.2 Beta';
