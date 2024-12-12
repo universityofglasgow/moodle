ccb7577ffbb10b565f974b2b5307a617b9fb552c
Greg Pedder <greg.pedder@glasgow.ac.uk>
2024-12-12 11:42:09 +0000

    Version bump for the January 2025 upgrade.

--- blocks/newgu_spdetails/version.php
+++ blocks/newgu_spdetails/version.php
@@ -25,6 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'block_newgu_spdetails';
-$plugin->version = 2024111801;
-$plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '0.2 Beta';
+$plugin->version = 2024121201;
+$plugin->requires = 2022041900; // Moodle 4.0.
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release  = '0.3 Beta';
--- local/gustaffview/version.php
+++ local/gustaffview/version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_gustaffview';
-$plugin->release = '0.2 Beta';
-$plugin->version = 2024111401;
-$plugin->requires = 2016112900;
+$plugin->version = 2024121201;
+$plugin->requires = 2022041900; // Moodle 4.0.
 $plugin->maturity = MATURITY_STABLE;
+$plugin->release = '0.3 Beta';
 
 $plugin->dependencies = [
     'block_newgu_spdetails' => ANY_VERSION
