2186334572e6bf48936653aee43a87df681e7188
Howard Miller <howardsmiller@gmail.com>
2025-03-31 12:05:24 +0100

    gu44: adding redis tool

---
+++ admin/tool/redis/version.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Admin tool "Redis management" - Version file
+ *
+ * @package    tool_redis
+ * @copyright  2020 Alexander Bias, Ulm University <alexander.bias@uni-ulm.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'tool_redis';
+$plugin->version = 2024060101;
+$plugin->release = 'v4.4-r2';
+$plugin->requires = 2024042200;
+$plugin->supported = [404, 404];
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = ['cachestore_redis' => 2024042200];
