9319494c3140a7c9a1b5320ba4b0774db6255a3e
Howard Miller <howardsmiller@gmail.com>
2025-06-20 11:07:06 +0100

    gu45: local_gugrades: bumping version as some significant changes

--- local/gugrades/version.php
+++ local/gugrades/version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version      = 2025051900;
+$plugin->version      = 2025062000;
 $plugin->requires     = 2022041900; // Moodle 4.0.
 $plugin->component    = 'local_gugrades';
 
