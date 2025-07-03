4ad7d75a9662b49f22853be6b7e3a1972361d17d
Ferenc-Frank-Lengyel <ferenc.lengyel@glasgow.ac.uk>
2025-07-03 11:07:33 +0100

    Re-installing Collapsed Topic course format v405.1.0

---
+++ course/format/topcoll/classes/output/courseformat/content/section/controlmenu.php
@@ -0,0 +1,137 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Contains the default section controls output class.
+ *
+ * @package    format_topcoll
+ * @copyright  &copy; 2021-onwards G J Barnard in respect to modifications of standard topics format.
+ * @author     G J Barnard - {@link https://moodle.org/user/profile.php?id=442195}
+ * @copyright  2020 Ferran Recio <ferran@moodle.com>
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace format_topcoll\output\courseformat\content\section;
+
+use core_courseformat\output\local\content\section\controlmenu as controlmenu_base;
+use context_course;
+use core\output\action_menu;
+use core\output\action_menu\link_secondary;
+use core\output\renderer_base;
+use core\output\pix_icon;
+use core\url;
+use stdClass;
+
+/**
+ * Base class to render a course section menu.
+ *
+ * @package   format_topics
+ * @copyright 2020 Ferran Recio <ferran@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class controlmenu extends controlmenu_base {
+    /** @var course_format the course format class */
+    protected $format;
+
+    /** @var section_info the course section class */
+    protected $section;
+
+    /**
+     * Generate the edit control items of a section.
+     *
+     * This method must remain public until the final deprecation of section_edit_control_items.
+     *
+     * @return array of edit control items
+     */
+    public function section_control_items() {
+
+        $format = $this->format;
+        $section = $this->section;
+        $course = $format->get_course();
+        $sectionreturn = $format->get_sectionnum();
+
+        $coursecontext = context_course::instance($course->id);
+
+        if ($sectionreturn) {
+            $url = $format->get_view_url($section->section, ['sr' => true]);
+        } else {
+            $url = $format->get_view_url($section->section);
+        }
+        $url->param('sesskey', sesskey());
+
+        $tcsettings = $format->get_settings();
+
+        $controls = [];
+        if (
+            (($tcsettings['layoutstructure'] == 1) || ($tcsettings['layoutstructure'] == 4)) &&
+            $section->section && has_capability('moodle/course:setcurrentsection', $coursecontext)
+        ) {
+            if ($course->marker == $section->section) {  // Show the "light globe" on/off.
+                $url->param('marker', 0);
+                $highlightoff = get_string('highlightoff');
+                $controls['highlight'] = [
+                    'url' => $url,
+                    'icon' => 'i/marked',
+                    'name' => $highlightoff,
+                    'pixattr' => ['class' => ''],
+                    'attr' => [
+                        'class' => 'editing_highlight',
+                        'data-action' => 'removemarker',
+                    ],
+                ];
+            } else {
+                $url->param('marker', $section->section);
+                $highlight = get_string('highlight');
+                $controls['highlight'] = [
+                    'url' => $url,
+                    'icon' => 'i/marker',
+                    'name' => $highlight,
+                    'pixattr' => ['class' => ''],
+                    'attr' => [
+                        'class' => 'editing_highlight',
+                        'data-action' => 'setmarker',
+                    ],
+                ];
+            }
+        }
+
+        $parentcontrols = parent::section_control_items();
+
+        $merged = [];
+        // If the edit key exists, we are going to insert our controls after it.
+        if (array_key_exists("edit", $parentcontrols)) {
+            // We can't use splice because we are using associative arrays.
+            // Step through the array and merge the arrays.
+            foreach ($parentcontrols as $key => $action) {
+                $merged[$key] = $action;
+                if ($key == "edit") {
+                    // If we have come to the edit key, merge these controls here.
+                    $merged = array_merge($merged, $controls);
+                }
+            }
+        } else {
+            // Put our controls first.
+            $merged = array_merge($controls, $parentcontrols);
+        }
+
+        // Alter 'permalink' to our form of url.
+        if (array_key_exists('permalink', $merged)) {
+            $merged['permalink']['url'] = $format->get_view_url($section->section, ['singlenavigation' => true]);
+        }
+
+        return $merged;
+    }
+}
