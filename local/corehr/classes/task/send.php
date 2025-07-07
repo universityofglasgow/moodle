fc77da1fd252512a3da3955797c17f45d770c96e
Howard Miller <howardsmiller@gmail.com>
2025-07-07 14:17:38 +0100

    gu45: MOOD-367: Working on Boomi integration

--- local/corehr/classes/task/send.php
+++ local/corehr/classes/task/send.php
@@ -45,7 +45,7 @@ class send extends \core\task\scheduled_task {
         } else {
 
             // Shiny new Boomi thing.
-            $message = $boomi->trainingrecord($status->coursecode, (int)$status->pesonnelno, $status->completed);
+            $message = $boomi->trainingrecord($status->coursecode, (int)$status->personnelno, $status->completed);
 
             return $message;
         }
