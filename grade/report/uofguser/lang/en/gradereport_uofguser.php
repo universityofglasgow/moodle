87c84cbc3b4f40ef09e3da0fe5feba1d57adddd2
Ferenc-Frank-Lengyel <ferenc.lengyel@glasgow.ac.uk>
2025-11-19 10:11:39 +0000

    UofG User grade report: last minute changes by TW: 1. inactive-active indicator colour, 2. check the course MyGrades enabled setting first, 3. name of the plugin, 4. placeholders for teachers

--- grade/report/uofguser/lang/en/gradereport_uofguser.php
+++ grade/report/uofguser/lang/en/gradereport_uofguser.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * English language pack for UofG User Report
+ * English language pack for Student MyGrades User Report (aka UofG User grade report)
  *
  * @package    gradereport_uofguser
  * @category   string
@@ -28,14 +28,19 @@ defined('MOODLE_INTERNAL') || die();
 $string['custommessage:allstudents'] = 'Custom message placeholder for teachers only, all students selected';
 $string['custommessage:noone'] = 'Custom message placeholder for teachers only, no one selected';
 $string['custommessage:student'] = 'Custom message placeholder for students only';
-$string['custommessage:teacherasmyself'] = 'Custom message placeholder for teachers only, viewing as teacher';
-$string['custommessage:teacherasstudent'] = 'Custom message placeholder for teachers only, viewing as student';
-$string['eventgradereportviewed'] = 'UofG User report viewed';
+$string['custommessage:teacherasmyself'] = 'This is a representative view of Student MyGrades for the selected student showing assessments,
+ grades and status as the student currently see\'s on Student MyGrades (exc Range & Feedback is simply a link on Student MyGrades)
+  and shows activities which might be <strong>hidden from the student</strong> also.
+   Note: The \'Source\' column is only visible to staff to highlight the source of grades populating to Student MyGrades.';
+$string['custommessage:teacherasstudent'] = 'This is a representative view of Student MyGrades for the selected student showing assessments,
+ grades and status as the student currently see\'s on Student MyGrades (exc Range & Feedback is simply a link on Student MyGrades).
+ Note: The \'Source\' column is only visible to staff to highlight the source of grades populating to Student MyGrades.';
+$string['eventgradereportviewed'] = 'Student MyGrades User Report viewed';
 $string['mygradesactive'] = 'MyGrades active';
 $string['mygradesenabled'] = 'MyGrades enabled';
 $string['mygradesinactive'] = 'MyGrades inactive';
-$string['pluginname'] = 'UofG User Report';
-$string['privacy:metadata'] = 'The UofG User Report plugin doesn\'t store any personal data.';
+$string['pluginname'] = 'Student MyGrades User Report';
+$string['privacy:metadata'] = 'The Student MyGrades User Report plugin doesn\'t store any personal data.';
 $string['showsource'] = 'Show source of the grade';
 $string['showsource_help'] = 'If enabled, the source of the grade (e.g., Moodle Gradebook or MyGrades) will be displayed in the report.';
 $string['showstatus'] = 'Show status of the assessment';
